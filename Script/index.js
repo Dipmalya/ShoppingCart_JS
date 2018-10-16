424db7351c14ec571dc89cd4ed51aa55b58deb50
Dipmalya <dipmalyasen@gmail.com>
2018-10-16 14:40:25 +0530

    Home page + Login done

---
+++ Script/index.js
@@ -0,0 +1,16 @@
+var validateUser = function() {
+  var flag = 0;
+  var uname = document.getElementById("userName").value;
+  var pwd = document.getElementById("password").value;
+  $.get("../Data/user.json", function(user, status) {
+    for (var i = 0; i < user.length; i++) {
+      if (user[i].email == uname && user[i].password == pwd) {
+        flag = 1;
+        window.location = "exam.html";
+      }
+    }
+    if (flag == 0)
+      document.getElementById("userAuth").innerHTML =
+        "Username or Password is invalid";
+  });
+};
