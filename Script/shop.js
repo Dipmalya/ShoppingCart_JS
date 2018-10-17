1db272bd9f03a96c1c38c998cd9d73bae27e4ae0
Dipmalya <dipmalyasen@gmail.com>
2018-10-17 20:47:44 +0530

    Shop categories done. + product data added + dynamic categories started

--- Script/shop.js
+++ Script/shop.js
@@ -1,15 +1,25 @@
 var allProducts = [];
+var categories = new Set();
 
 var bringProducts = function() {
   $.get("../Data/product.json", function(product, status) {
     allProducts = product;
+    selectMobile();
   });
 };
 
 var selectMobile = function() {
   for (var product of allProducts) {
-    if (product.category === "Mobile") {
-      //console.log(product.name);
-    }
+    categories.add(product.category);
   }
+  categories = Array.from(categories);
+  var ul = document.getElementById("navTabs");
+  for (var item of categories) {
+    console.log(item);
+    var li = document.createElement("li");
+    li.className = item;
+    li.innerHTML = (item + "").toUpperCase();
+    ul.appendChild(li);
+  }
+  //console.log(categories[0]);
 };
