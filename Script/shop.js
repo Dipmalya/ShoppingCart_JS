b0539ec425c21dcb87dde4d44e6fce131e411f6d
Dipmalya <dipmalyasen@gmail.com>
2018-10-17 12:39:56 +0530

    Shop categories done. + product data added..

--- Script/shop.js
+++ Script/shop.js
@@ -1,7 +1,15 @@
+var allProducts = [];
 
 var bringProducts = function() {
   $.get("../Data/product.json", function(product, status) {
-    
+    allProducts = product;
   });
 };
 
+var selectMobile = function() {
+  for (var product of allProducts) {
+    if (product.category === "Mobile") {
+      //console.log(product.name);
+    }
+  }
+};
