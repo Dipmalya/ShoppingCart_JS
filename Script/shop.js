7cd31297cc65ed5f67e959dbce6fa62621e14365
Dipmalya <dipmalyasen@gmail.com>
2018-10-18 17:47:02 +0530

    Filter side bar & item displaying dynamically

--- Script/shop.js
+++ Script/shop.js
@@ -1,8 +1,15 @@
-var allProducts = [];
-var categories = new Set();
-var sub_categories = new Map();
-var currentUser = localStorage.getItem("present-user");
+var allProducts = []; //array stores all the data
+var categories = new Set(); //set to store the categories
+var sub_categories = new Map(); //map to map the categories with the sub-categories
+var currentUser = localStorage.getItem("present-user");   //stores the name of the user logged-in
 
+/*
+    @author : Dipmalya Sen
+    @desc : This function loads the data initially from the json file,
+            calls the function to load the nav bar as per categories available,
+            display the items,
+            fetches the name of the user logged-in
+*/
 var bringProducts = function() {
   $.get("../Data/product.json", function(product, status) {
     allProducts = product;
@@ -16,6 +23,10 @@ var bringProducts = function() {
   });
 };
 
+/*
+    @author : Dipmalya Sen
+    @desc : This function creates the nav-bar dynamically based on the categories available in the file
+*/
 var setCategories = function() {
   for (var product of allProducts) {
     categories.add(product.category);
@@ -26,6 +37,7 @@ var setCategories = function() {
 
   var ul = document.getElementById("navTabs");
 
+  //creating the nav bar tabs
   for (var item of categories) {
     var li = document.createElement("li");
     li.className = item;
@@ -35,6 +47,7 @@ var setCategories = function() {
     li.appendChild(sub_ul);
     ul.appendChild(li);
 
+    //creating the drop-down sub items
     for (var i of sub_categories) {
       if (i[1] == item) {
         var sub_li = document.createElement("li");
@@ -46,12 +59,21 @@ var setCategories = function() {
   }
 };
 
+/*
+    @author : Dipmalya Sen
+    @desc : This function toggles the filter menu
+*/
 var displayFilter = function() {
   if ($("#sub-panel").css("display") == "none")
     $("#sub-panel").css("display", "block");
   else $("#sub-panel").css("display", "none");
 };
 
+/*
+    @author : Dipmalya Sen
+    @desc : This function dynamically fetches the data from the json file,
+            adds the image and name
+*/
 var displayItems = function() {
   for (var product of allProducts) {
     console.log(product);
