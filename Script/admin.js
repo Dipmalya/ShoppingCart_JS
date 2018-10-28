96f7ba6c80cacbf53613b96674bc53d4bd62f148
Dipmalya <dipmalyasen@gmail.com>
2018-10-28 18:26:10 +0530

    admin module integrated

---
+++ Script/admin.js
@@ -0,0 +1,85 @@
+var allProducts = []; //array stores all the data
+var restLink = "http://localhost:4010/rest/api/delete/";
+
+var bringProducts = function() {
+  $.get("http://localhost:4010/rest/api/get", function(product, status) {
+    allProducts = product;
+
+    fillTable(allProducts);
+  });
+};
+
+var fillTable = function(products) {
+  for (var product of products) {
+    var tRow = document.createElement("tr");
+    tRow.setAttribute("id", product.id);
+    document.getElementById("tData").appendChild(tRow);
+
+    var tD = document.createElement("td");
+    tD.innerHTML = product.id;
+    document.getElementById(product.id).appendChild(tD);
+
+    var tD = document.createElement("td");
+    tD.innerHTML = product.name;
+    document.getElementById(product.id).appendChild(tD);
+
+    var tD = document.createElement("td");
+    tD.innerHTML = product.sub_category;
+    document.getElementById(product.id).appendChild(tD);
+
+    var tD = document.createElement("td");
+    tD.innerHTML = product.brand;
+    document.getElementById(product.id).appendChild(tD);
+
+    var tD = document.createElement("td");
+    tD.innerHTML = "Rs. " + product.price;
+    document.getElementById(product.id).appendChild(tD);
+
+    var btnEdit = document.createElement("button");
+    btnEdit.setAttribute("class", "btn btn-primary");
+    btnEdit.setAttribute("id", "btnEdit" + product.id);
+    btnEdit.innerHTML = "Edit";
+    document.getElementById(product.id).appendChild(btnEdit);
+
+    var btnDel = document.createElement("button");
+    btnDel.setAttribute("class", "btn btn-danger");
+    btnDel.setAttribute("id", "btnDel" + product.id);
+    btnDel.innerHTML = "Remove";
+    btnDel.addEventListener("click", () => {
+      var id = window.event.target.id + "";
+      id = id.slice(-3);
+      $.ajax({
+        url: restLink + id,
+        type: "DELETE",
+        success: function(response) {
+          window.location = "./admin.html";
+        }
+      });
+    });
+    document.getElementById(product.id).appendChild(btnDel);
+  }
+};
+
+var productArr = [];
+$(document).ready(function() {
+  $("#searchId").keyup(() => {
+    var value = document.getElementById("searchId").value;
+    for (var p of allProducts) {
+      if (
+        p.name
+          .toString()
+          .toUpperCase()
+          .indexOf(value.toUpperCase()) !== -1
+      ) {
+        productArr.push(p);
+        console.log(p.id);
+      }
+    }
+    //console.log(productArr);
+    //fillTable(productArr);
+  });
+});
+
+var logOut = function() {
+  window.location = "./index.html";
+};
--- Script/index.js
+++ Script/index.js
@@ -7,16 +7,21 @@ var validateUser = function() {
   var flag = 0;
   var uname = document.getElementById("userName").value;
   var pwd = document.getElementById("password").value;
-  $.get("http://localhost:4010/rest/api-user/get", function(user, status) {
-    for (var i = 0; i < user.length; i++) {
-      if (user[i].email == uname && user[i].password == pwd) {
-        flag = 1;
-        localStorage.setItem("present-user", user[i].name);
-        window.location = "./home.html";
+  if (uname == "admin" && pwd == "admin") {
+    flag = 1;
+    window.location = "./admin.html";
+  } else {
+    $.get("http://localhost:4010/rest/api-user/get", function(user, status) {
+      for (var i = 0; i < user.length; i++) {
+        if (user[i].email == uname && user[i].password == pwd) {
+          flag = 1;
+          localStorage.setItem("present-user", user[i].name);
+          window.location = "./home.html";
+        }
       }
-    }
-    if (flag == 0)
-      document.getElementById("userAuth").innerHTML =
-        "Username or Password is invalid";
-  });
+      if (flag == 0)
+        document.getElementById("userAuth").innerHTML =
+          "Username or Password is invalid";
+    });
+  }
 };
